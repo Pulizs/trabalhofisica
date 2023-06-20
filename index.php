3aa1a29fd75e36d9865a9165e0f646f25e20cbfa
Pulizs <68717923+Pulizs@users.noreply.github.com>
2023-06-20 00:44:06 -0300

    create project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <title>Rendimento</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            
+        </div>
+    </nav>
+    <div class="container">
+
+    </div>
+</body>
+</html>
