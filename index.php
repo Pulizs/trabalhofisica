aeec7d4915f199a7133f0330c60699df01ca282d
Pulizs <68717923+Pulizs@users.noreply.github.com>
2023-06-20 09:14:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,14 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            
+            <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
         </div>
     </nav>
     <div class="container">
-
+        <input type="text">
     </div>
 </body>
 </html>
